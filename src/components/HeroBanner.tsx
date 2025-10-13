19708242971e5691b52bbc812c90d6898b25cbff
kadsnandjsaif <107490810+kadsnandjsaif@users.noreply.github.com>
2025-10-13 23:18:15 +0400

    fix3r34

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -4,16 +4,16 @@ import Image from 'next/image';
 const HeroBanner = () => {
   return (
     <div className="w-full flex justify-center px-4 py-1  bg-white">
-      <div className="w-full max-w-[1400px] relative">
+      <div className="w-full max-w-[1200px] relative">
         
         <div className="relative w-full" style={{ aspectRatio: '9/12' }}> {/* Укажите правильное соотношение */}
           <Image
-            src="/katalog.jpg"
+            src="/katalog.png"
             alt="Katalog"
             fill
             className="object-contain" // Показывает все изображение без обрезки
             priority
-            sizes="(max-width: 1400px) 100vw, 1400px"
+            sizes="(max-width: 1200px) 100vw, 1200px"
           />
         </div>
       </div>
